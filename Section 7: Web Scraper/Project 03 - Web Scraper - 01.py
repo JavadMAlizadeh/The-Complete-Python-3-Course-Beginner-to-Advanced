99099bded6a5d356d4f725de83386a4793db01d8
Javad Mohammad Alizadeh <javad.mohammadalizadeh@gmail.com>
2021-02-09 13:58:38 +0330

    Delete Section 7: Web Scraper directory

--- Section 7: Web Scraper/Project 03 - Web Scraper - 01.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-search = input("Enter search term:")
-params = {"q": search}
-r = requests.get("http://www.bing.com/search", params=params)
-
-soup = BeautifulSoup (r.text, "html.parser")
-print(soup.prettify())
