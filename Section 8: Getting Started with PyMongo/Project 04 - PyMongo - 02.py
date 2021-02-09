aee78c2eb393ce5c73892a86bc4630c7d8e036db
Javad Mohammad Alizadeh <javad.mohammadalizadeh@gmail.com>
2021-02-09 14:00:37 +0330

    Delete Section 8: Getting Started with PyMongo directory

--- Section 8: Getting Started with PyMongo/Project 04 - PyMongo - 02.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-
-client = MongoClient()
-db = client.mydb
-Users = db.users
-Users.find().count()
-Users.find({"password": "bbff", "username": "sara"}).count()
